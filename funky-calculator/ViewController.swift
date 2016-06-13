ab9c074734a8f87c3ba4e5926520655ce210931e
Diego Llamas <diegollams@gmail.com>
2016-06-13 16:49:03 -0500

    Basic functions in calculator

--- funky-calculator/ViewController.swift
+++ funky-calculator/ViewController.swift
@@ -15,24 +15,68 @@ class ViewController: UIViewController {
     var displayValue = 0.0
     var leftNumber:Double?
     var calculatedOperation = false
-    
+    var operation = ""
     override func viewDidLoad() {
+        loadDisplayLabel()
+    }
+    
+    func makeOperation(){
+        if leftNumber == nil {
+            leftNumber = displayValue
+            calculatedOperation = true
+        }
+        else{
+            switch operation {
+            case "+":
+                displayValue += leftNumber!
+                break
+            case "-":
+                displayValue -= leftNumber!
+                break
+            case "/":
+                if !(displayValue == 0){
+                    displayValue /= leftNumber!
+                }
+                else{
+                    let errorAlert = UIAlertController(title: "Error", message: "Division by 0.", preferredStyle: UIAlertControllerStyle.Alert)
+                    errorAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in
+                        self.clearValues()
+                    }))
+                    presentViewController(errorAlert, animated: true, completion: nil)
+                }
+                break
+            case "*":
+                displayValue *= leftNumber!
+                break
+            default: break
+                
+            }
+            leftNumber = displayValue
+            calculatedOperation = true
+
+        }
         loadDisplayLabel()
     }
     
     func clearValues(){
         displayValue = 0
-        leftNumber = 0
+        leftNumber = nil
         calculatedOperation = false
+        operation = ""
     }
     
     func loadDisplayLabel(){
         displayLabel.text = "\(displayValue)"
     }
 
+    @IBAction func equalPress(button: UIButton){
+        makeOperation()
+    }
+    
     @IBAction func numberPressed(button: UIButton){
         if calculatedOperation{
-            clearValues()
+            displayValue = Double(button.tag)
+            calculatedOperation = false
         }
         else{
             displayValue = (displayValue * 10.0) + Double(button.tag)
@@ -46,15 +90,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func sumPress(button: UIButton){
-        if (leftNumber == nil) {
-            leftNumber = displayValue
-            displayValue = 0.0
-        }else{
-            displayValue += leftNumber!
-            leftNumber = displayValue
-            calculatedOperation = true
-        }
-        loadDisplayLabel()
+        operation = "+"
+        makeOperation()
+        
+    }
+    
+    @IBAction func subtractionPress(button: UIButton){
+        operation = "-"
+        makeOperation()
+    }
+    
+    @IBAction func multiplicationPress(button: UIButton){
+        operation = "*"
+        makeOperation()
+    }
+    
+    @IBAction func dividePress(button: UIButton){
+        operation = "/"
+        makeOperation()
     }
     
     
